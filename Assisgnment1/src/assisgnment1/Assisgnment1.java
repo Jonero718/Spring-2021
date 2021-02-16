5557d76b50225284c26f2f24734706eb05f69212
bxray <bxray@Computadora.local>
2021-02-15 21:19:02 -0500

    Submitting my first assignment.

---
+++ Assisgnment1/src/assisgnment1/Assisgnment1.java
@@ -0,0 +1,20 @@
+
+package assisgnment1;
+
+public class Assisgnment1 {
+
+  
+    public static void main(String[] args) {
+       
+         for(int i=0; i<=100 ;i+=2)
+        // For loop that initalizes i as 0, checks that i is less than or equal 
+        // too 100, and add 2 two everytime the loop is initialized until is i 
+        // is less than or equal too 100
+        {          
+           
+            System.out.println(i);
+        // Prints out i
+         }
+    }
+    
+}
